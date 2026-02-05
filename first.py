66853e35a026539e3c09d23d3a0d233ee3f4f201
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-05 12:14:20 -0500

    Variables

--- first.py
+++ first.py
@@ -1,2 +1,6 @@
 # This is a comment, the interpreter ignores this line
 print("Sup cuh?")
+# variable, dynamically typed, typing becomes set after equal sign, can be changed
+name = "Marshall"
+name = 67
+print("Hello", name)
