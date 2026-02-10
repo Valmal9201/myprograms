d62070c9b0131c48fe945de6604f77346a84e458
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-10 11:13:52 -0500

    updated notes :D

--- first.py
+++ first.py
@@ -7,27 +7,35 @@ print("Hello", name)
 
 # Pleaase open your "myprograms" folder on Visual Studio Code
 # First legit program :)
+
 # Variable
 # named as message, lowercase, no # start, uppercase reserved for something else, no swear words and meaningful names
 # no specific type, dynamic & changes by situation and context, no semi colon to end code, press enter instead
+
 # = Sign
 # "=" = assignment/operator, left = variable, right = value
+
 # Values
 #"Hello, welcome to ISC4U" = value or data
+
 # Types of Data Types
 # int = integer, float = +/- decimal, bool = boolean (True/False), str = String: Alpha, numeric, special symbols
 # list = data type that can hold many other data types (java arraylist w/o restrictions), dict = dictionary
 message = "Hello, welcome to ISC4U."
+
 # Input
 # Example of: built in function, stores String input from terminal to the related variable, assign to "name"
 name = input("What is your name?: ")
+
 # , makes space when print, end = "\n", sep = " "
 print(message, name, end = "\n", sep = " ")
 print ("Stephen, I hope this answers your question.")
+
 # cmd + / = comment all
 num1 = 6
 num2 = 7
 print(num1 + num2)
+
 # Input
 # reads/understands all inputs as Str
 # int() convert input as integer, input 1st, int() convert
