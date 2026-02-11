980b33c725f977b8d99bc82b015429038da1122b
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-11 11:27:07 -0500

    Problem in class

---
+++ solution.py
@@ -0,0 +1,16 @@
+# string1 = input("Enter your first string: ")
+# string2 = input("Enter your second string: ")
+
+# if (len(string1) == len(string2) or len(string1)<len(string2)):
+#     print("The longer word is:", string2)
+# else:
+#     print("The longer word is:", string1)
+
+# Longer word~
+word = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+if len(word) > len(word2):
+    print(word)
+else:
+    print(word2)
