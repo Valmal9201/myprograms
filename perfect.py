8d52bbea5be249dae409b2f29ba4c9e8900139eb
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-19 11:27:58 -0500

    Perfect Number Problem My & In Class Solution

--- perfect.py
+++ perfect.py
@@ -1,26 +1,28 @@
 # Abundant, Deficient, Perfect? My Solution
-# import math
-# num = int(input("Enter a number: "))
-# stop = int(math.sqrt(num)) + 1
-# isPrime = True
-# divider = 2
-# sum = 0
-# while divider < stop:
-#     if num % divider == 0:
-#         quotient = num // divider
-#         sum = sum + divider + quotient
-#     divider += 1
-# #Output
-# sum += 1
-# if sum == num:
-#     print(f"{num} is a perfect number.")
-# elif sum < num:
-#     print(f"{num} is a deficient number.")
-# else:
-#     print(f"{num} is a abundant number.")
+import math
+num = int(input("Enter a number: "))
+stop = int(math.sqrt(num)) + 1
+isPrime = True
+divider = 2
+sum = 0
+
+while divider < stop:
+    if num % divider == 0:
+        quotient = num // divider
+        sum = sum + divider + quotient
+    divider += 1
+# Output
+sum += 1
+
+if sum == num:
+    print(f"{num} is a perfect number.")
+elif sum < num:
+    print(f"{num} is a deficient number.")
+else:
+    print(f"{num} is a abundant number.")
 
 # Perfect Numbers Solutions in Class Solution
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
 def numType(num):
     # Doc String, 3 """ 1st line for purpose, define variable,
