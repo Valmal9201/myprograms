fb25334b2752d91966130c0d787f1b48f1932a01
Valmal9201 <zeng.cynthia236@gmail.com>
2026-03-24 11:50:31 -0400

    Bubble, Destruction Selection, Insertion Sort Algorithms

--- sort_algo.py
+++ sort_algo.py
@@ -2,6 +2,7 @@ a_list = ["e", "d", "f", "b", "a", "g", "c"]
 # In Class Solution
 # Thinking about sorting :)
 # Bubble Sort
+print(a_list)
 def bubble(a_list):
     # assume a_list is unsorted :) least to greatest
     swapped = True
@@ -17,21 +18,26 @@ def bubble(a_list):
     return a_list
 print(bubble(a_list))
 
-# No Return b/c not change value just order
-#In class
-def destruction_select(a_list):
-    b_list = []
-    while a_list:
-        smoll = min(a_list)
-        b_list.append(smol)
-        a_list.remove(smol)
-    return b_list
+# # No Return b/c not change value just order
+# #In class
+# a_list = ["e", "d", "f", "b", "a", "g", "c"]
+# def destruction_select(a_list):
+#     b_list = []
+#     while a_list:
+#         smoll = min(a_list)
+#         b_list.append(smol)
+#         a_list.remove(smol)
+#     return b_list
+# print(bubble(a_list))
 
-def insertion(a_list):
-    for i in range(len(a_list) - 1):
-        for j in range(i + 1, 0, - 1):
-            if a_list[j-  1] > a_list[j]:
-                a_list[j - 1], a_list[j] = a_list[j], a_list[j - 1]
-                print(a_list)
-            else:
-                break
+# a_list = ["e", "d", "f", "b", "a", "g", "c"]
+# def insertion(a_list):
+#     for i in range(len(a_list) - 1):
+#         for j in range(i + 1, 0, - 1):
+#             if a_list[j-  1] > a_list[j]:
+#                 a_list[j - 1], a_list[j] = a_list[j], a_list[j - 1]
+#                 print(a_list)
+#             else:
+#                 break
+#     return a_list
+# print(insertion(a_list))
